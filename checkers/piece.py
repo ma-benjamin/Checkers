813ec47236f54f60ebb5405003d13647fe76ec68
Benjamin Ma <ben1002.ma@gmail.com>
2023-02-02 22:03:44 -0500

    finishing checkers game

--- checkers/piece.py
+++ checkers/piece.py
@@ -1,8 +1,8 @@
 import pygame
-from .constants import RED, WHITE, GREY, SQUARE_SIZE
+from .constants import RED, WHITE, GREY, SQUARE_SIZE, CROWN
 
 class Piece:
-  PADDING = 10
+  PADDING = 15
   OUTLINE = 2
 
   def __init__(self, row, col, color):
@@ -10,14 +10,9 @@ class Piece:
     self.col = col
     self.color = color
     self.king = False
-    
-    if self.color == RED:
-      self.direction = -1
-    else: 
-      self.direction = 1
-
     self.x = 0
     self.y = 0
+    self.calc_pos()
 
   def calc_pos(self):
     self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
@@ -30,6 +25,14 @@ class Piece:
     radius = SQUARE_SIZE//2 - self.PADDING
     pygame.draw.circle(win, GREY, (self.x, self.y), radius + self.OUTLINE)
     pygame.draw.circle(win, self.color, (self.x, self.y), radius)
+    if self.king:
+      win.blit(CROWN, (self.x - CROWN.get_width()//2, self.y - CROWN.get_height()//2))
+
+  def move(self, row, col):
+    self.row = row
+    self.col = col
+    self.calc_pos()
 
   def __repr__(self):
     return str(self.color)
+
